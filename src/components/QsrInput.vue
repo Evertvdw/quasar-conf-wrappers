37ef55490117344b7d78ae3896c5c23a51aef84e
Evert van der Weit <evertvanderweit@gmail.com>
2022-05-30 21:10:59 +0200

    Setting up example

---
+++ src/components/QsrInput.vue
@@ -0,0 +1,52 @@
+<template>
+  <q-input
+    @update:model-value="$emit('update:model-value', $event)"
+    :model-value="modelValue"
+    class="qsr-input"
+    label="Name"
+    outlined
+    no-error-icon
+    lazy-rules
+    hide-bottom-space
+    :rules="[
+      (val) => !!val || 'Name is required',
+      (val) => val.length > 3 || `We don't like short names`,
+    ]"
+  />
+</template>
+
+<script setup lang="ts">
+defineProps<{
+  modelValue: string;
+}>();
+
+defineEmits<{
+  (event: 'update:model-value', value: string | number | null): void;
+}>();
+</script>
+
+<style lang="scss">
+.qsr-input {
+  .q-field__label {
+    transition: transform 150ms cubic-bezier(0.4, 0, 0.2, 1),
+      font-size 150ms cubic-bezier(0.4, 0, 0.2, 1);
+    z-index: 1;
+    background-color: white;
+    padding-left: 6px;
+    padding-right: 6px;
+    margin-left: -6px;
+  }
+
+  &.q-field--float {
+    .q-field__label {
+      transform: translateY(-138%);
+      font-size: 12px;
+    }
+  }
+
+  .q-field__native {
+    padding-top: 8px;
+    font-size: 16px;
+  }
+}
+</style>
--- src/pages/IndexPage.vue
+++ src/pages/IndexPage.vue
@@ -1,49 +1,60 @@
 <template>
   <q-page class="row items-center justify-evenly">
-    <example-component
-      title="Example component"
-      active
-      :todos="todos"
-      :meta="meta"
-    ></example-component>
+    <q-form class="q-gutter-md" style="width: 300px">
+      <h4 class="q-mb-md">Awesome form</h4>
+      <qsr-input v-model="form.name" />
+      <q-input
+        v-model="form.name"
+        label="Name"
+        outlined
+        no-error-icon
+        lazy-rules
+        hide-bottom-space
+        :rules="[
+          (val) => !!val || 'Name is required',
+          (val) => val.length > 3 || `We don't like short names`,
+        ]"
+      />
+      <q-input
+        v-model="form.profession"
+        label="Profession"
+        outlined
+        lazy-rules
+        hide-bottom-space
+        no-error-icon
+        :rules="[(val) => !!val || 'Profession is required']"
+      />
+      <q-input
+        v-model="form.hobbies"
+        label="Hobbies"
+        outlined
+        stack-label
+        lazy-rules
+        hide-bottom-space
+        no-error-icon
+      />
+      <q-input
+        v-model="form.favorite_food"
+        label="Favorite food"
+        outlined
+        stack-label
+        lazy-rules
+        hide-bottom-space
+        no-error-icon
+      />
+      <q-btn type="submit" color="primary" label="Submit" />
+    </q-form>
   </q-page>
 </template>
 
-<script lang="ts">
-import { Todo, Meta } from 'components/models';
-import ExampleComponent from 'components/ExampleComponent.vue';
-import { defineComponent, ref } from 'vue';
+<script setup lang="ts">
+import { reactive } from 'vue';
+import QsrInput from 'src/components/QsrInput.vue';
 
-export default defineComponent({
-  name: 'IndexPage',
-  components: { ExampleComponent },
-  setup () {
-    const todos = ref<Todo[]>([
-      {
-        id: 1,
-        content: 'ct1'
-      },
-      {
-        id: 2,
-        content: 'ct2'
-      },
-      {
-        id: 3,
-        content: 'ct3'
-      },
-      {
-        id: 4,
-        content: 'ct4'
-      },
-      {
-        id: 5,
-        content: 'ct5'
-      }
-    ]);
-    const meta = ref<Meta>({
-      totalCount: 1200
-    });
-    return { todos, meta };
-  }
+const form = reactive({
+  name: '',
+  profession: '',
+  hobbies: '',
+  favorite_food: '',
 });
 </script>
