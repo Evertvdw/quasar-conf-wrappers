89204254472f5a343bed2d139d66b352c83c6aea
Evert van der Weit <evertvanderweit@gmail.com>
2022-06-01 20:01:36 +0200

    Option 1 worked out

--- src/components/_wrappers/QsrInput.vue
+++ src/components/_wrappers/QsrInput.vue
@@ -1,5 +1,6 @@
 <template>
   <q-input
+    ref="inputRef"
     @update:model-value="$emit('update:model-value', $event)"
     :model-value="modelValue"
     class="qsr-input"
@@ -7,35 +8,54 @@
     no-error-icon
     lazy-rules
     hide-bottom-space
-  />
+  >
+    <template v-for="(_, slot) in $slots" v-slot:[slot]="scope">
+      <slot :name="slot" v-bind="scope || {}" />
+    </template>
+  </q-input>
 </template>
 
 <script setup lang="ts">
-import { QInputProps } from 'quasar';
+import { QInput, QInputProps } from 'quasar';
 
 // Cannot directly pass imported interface to defineProps
 // see https://github.com/vuejs/core/issues/4294
-// There is a plugin to use in the meantime: https://github.com/wheatjs/vite-plugin-vue-type-imports
-// Or declare your own interface and extend the imported one as a workaround like below
+// Declare your own interface and extend the imported one as a workaround like below
 
 // eslint-disable-next-line @typescript-eslint/no-empty-interface
 interface QsrInput extends QInputProps {
   // If we do not provide this Vue throws a warning that modelValue is not supplied
+  // Every prop explicitely used needs to be defined here like above
   modelValue: QInputProps['modelValue'];
+
+  // Additional custom prop
+  /**
+   * Animation speed of the label moving up
+   * @defuault 150ms
+   */
+  animationSpeed?: string;
 }
 
-defineProps<QsrInput>();
+withDefaults(defineProps<QsrInput>(), {
+  animationSpeed: '150ms',
+});
 
 defineEmits<{
   (event: 'update:model-value', value: string | number | null): void;
 }>();
+
+const inputRef = ref<QInput | null>(null);
+
+defineExpose({
+  input: inputRef,
+});
 </script>
 
 <style lang="scss">
 .qsr-input {
   .q-field__label {
-    transition: transform 150ms cubic-bezier(0.4, 0, 0.2, 1),
-      font-size 150ms cubic-bezier(0.4, 0, 0.2, 1);
+    transition: transform v-bind('animationSpeed') cubic-bezier(0.4, 0, 0.2, 1),
+      font-size v-bind('animationSpeed') cubic-bezier(0.4, 0, 0.2, 1);
     z-index: 1;
     background-color: white;
     padding-left: 6px;
@@ -45,7 +65,7 @@ defineEmits<{
 
   &.q-field--float {
     .q-field__label {
-      transform: translateY(-138%);
+      transform: translateY(-136%);
       font-size: 12px;
     }
   }
--- src/pages/IndexPage.vue
+++ src/pages/IndexPage.vue
@@ -27,27 +27,32 @@
         v-model="form.hobbies"
         label="Hobbies"
         outlined
-        stack-label
         lazy-rules
         hide-bottom-space
         no-error-icon
+        ref="formHobbies"
       />
       <q-input
         v-model="form.favorite_food"
         label="Favorite food"
         outlined
-        stack-label
         lazy-rules
         hide-bottom-space
         no-error-icon
-      />
+      >
+        <template #prepend>
+          <q-icon name="local_pizza" />
+        </template>
+      </q-input>
       <q-btn type="submit" color="primary" label="Submit" />
+      <q-btn color="primary" outline label="Focus hobbies" @click="onClick" />
     </q-form>
     <q-form class="q-gutter-md" style="width: 300px">
       <h4 class="q-mb-md">Better form</h4>
       <qsr-input
         v-model="form2.name"
         label="Name"
+        animation-speed="500ms"
         :rules="[
           (val) => !!val || 'Name is required',
           (val) => val.length > 3 || `We don't like short names`,
@@ -59,16 +64,20 @@
         :rules="[(val) => !!val || 'Profession is required']"
         :no-error-icon="false"
       />
-      <qsr-input v-model="form2.hobbies" label="Hobbies" />
-      <qsr-input v-model="form2.favorite_food" label="Favorite food" />
+      <qsr-input v-model="form2.hobbies" label="Hobbies" ref="form2Hobbies" />
+      <qsr-input v-model="form2.favorite_food" label="Favorite food">
+        <template #prepend>
+          <q-icon name="local_pizza" />
+        </template>
+      </qsr-input>
       <q-btn type="submit" color="primary" label="Submit" />
+      <q-btn color="primary" outline label="Focus hobbies" @click="onClick2" />
     </q-form>
   </q-page>
 </template>
 
 <script setup lang="ts">
-import { reactive } from 'vue';
-import QsrInput from 'src/components/QsrInput.vue';
+import { QInput } from 'quasar';
 
 const form = reactive({
   name: '',
@@ -76,6 +85,10 @@ const form = reactive({
   hobbies: '',
   favorite_food: '',
 });
+const formHobbies = ref<QInput | null>(null);
+function onClick() {
+  formHobbies?.value?.focus();
+}
 
 const form2 = reactive({
   name: '',
@@ -83,4 +96,10 @@ const form2 = reactive({
   hobbies: '',
   favorite_food: '',
 });
+// Notice we get an object here which is equal to the defineExpose we set inside QsrInput
+// So the key name 'input' is something we define ourselves
+const form2Hobbies = ref<{ input: QInput } | null>(null);
+function onClick2() {
+  form2Hobbies?.value?.input.focus();
+}
 </script>
